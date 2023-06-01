5cfddeffd2ee500feda2b6e5cde60304d5e592b9
GLD5000 <garethlouisdevlin@gmail.com>
2023-06-01 13:22:04 +0100

    Added nodemon

--- index.js
+++ index.js
@@ -2,35 +2,31 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  console.log("hello");
-  res.sendFile(__dirname + '/views/index.html');
-
+  console.log("helloxxxx");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({unix: 'hello API', utc: 'hello API'});
+  res.json({ unix: "hello API", utc: "hello API" });
 });
 
-app.get("/", function (req, res) {
-});
-
+app.get("/", function (req, res) {});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
