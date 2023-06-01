ce03ea367ae57d06c719ea6eb67ed0361a774c82
GLD5000 <garethlouisdevlin@gmail.com>
2023-06-01 16:16:08 +0100

    Updated return

--- index.js
+++ index.js
@@ -15,18 +15,40 @@ app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  console.log("helloxxxx");
+  console.log("helloxxxsdvsodmvsx");
   res.sendFile(__dirname + "/views/index.html");
 });
 
 // your first API endpoint...
-app.get("/api/hello", function (req, res) {
-  res.json({ unix: "hello API", utc: "hello API" });
+app.get("/api/:date?", function (req, res) {
+  const stringIn = req.params.date;
+  const newDate = new Date(stringIn);
+  const isInvalidDate = newDate.toString() === "Invalid Date";
+  if (isInvalidDate) return res.json({ error : "Invalid Date" });
+  const utc = newDate.toUTCString();
+  const unix = newDate.getTime();
+console.log('utc:', utc);
+console.log('unix:', unix);
+  // console.log("isDateString:", isDateString);
+  // console.log("new Date(stringIn):", new Date(stringIn));
+  // console.log("To UTCstring:", new Date(stringIn).toUTCString());
+  // console.log("getTime:", new Date(stringIn).getTime());
+  // console.log("Date.parse():", Date.parse(new Date(stringIn)));
+  // console.log(
+  //   "UTX To UTCstring:",
+  //   new Date(Date.parse(new Date(stringIn))).toUTCString()
+  // );
+  // console.log("fudgeddate:", new Date("fudgeddate"));
+  // console.log(
+  //   "fudgeddate:",
+  //   "Invalid Date" === new Date("fudgeddate").toString()
+  // );
+  res.json({ unix: unix, utc: utc });
 });
 
 app.get("/", function (req, res) {});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 5000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
